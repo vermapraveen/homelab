67340c149e691fb849681cc7468597374bb49692
praveen@alpha <alpha@praveenkv.com>
2021-01-26 07:34:49 -0600

    OK

--- command.sh
+++ command.sh
@@ -3,5 +3,5 @@
 # ansible-playbook ./playbooks/02-k8-config.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
 # ansible-playbook ./playbooks/03-k8-basic-tools.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
 # ansible-playbook ./playbooks/04-k8-config-after-tool.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-ansible-playbook ./playbooks/05-k8-config-master.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/06-k8-config-worker.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/05-k8-config-master.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+ansible-playbook ./playbooks/06-k8-config-worker.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
