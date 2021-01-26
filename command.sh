7a1fba0fe3cec5d5fc5f0484cea46e3d82a61c50
praveen@alpha <alpha@praveenkv.com>
2021-01-26 07:03:46 -0600

    works ok till 04

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
 #ansible -i ./inventory/hosts ubuntu -m ping -u pkv --ask-pass
 #ansible-playbook ./playbooks/apt.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-ansible-playbook ./playbooks/basic-tools.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/02-k8-config.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/03-k8-basic-tools.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/04-k8-config-after-tool.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+ansible-playbook ./playbooks/05-k8-config-master.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/06-k8-config-worker.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
