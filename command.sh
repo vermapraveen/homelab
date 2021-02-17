2aed51b151f1cfc83bccfd4cc5e81298fd041778
praveen@alpha <alpha@praveenkv.com>
2021-02-17 17:52:25 -0600

    completed

--- command.sh
+++ command.sh
@@ -1,13 +1,15 @@
 # ansible -i ./inventory/hosts ubuntu -m ping -u pkv --ask-pass
-ansible-playbook ./playbooks/deploy-ssh.yml -i ./inventory/hosts -u pkv 
+ansible-playbook ./playbooks/02-k8-config.yml -i ./inventory/hosts -u pkv
+ansible-playbook ./playbooks/05-k8-config-master.yml -i ./inventory/hosts -u pkv
+ansible-playbook ./playbooks/06-k8-config-worker.yml -i ./inventory/hosts -u pkv 
+kubernetes/setups/lb/lb.sh
+kubectl apply -f deploy.yml
 
-# ansible-playbook ./playbooks/apt.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/01-setup-ns.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/02-k8-config.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/03-k8-basic-tools.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/04-k8-config-after-tool.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/05-k8-config-master.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
-# ansible-playbook ./playbooks/06-k8-config-worker.yml -u pkv --ask-pass --ask-become-pass -i ./inventory/hosts
+# ansible-playbook ./playbooks/deploy-ssh.yml -i ./inventory/hosts -u pkv 
+
+# ansible-playbook ./playbooks/apt.yml -u pkv -i ./inventory/hosts
+# ansible-playbook ./playbooks/01-setup-ns.yml -u pkv -i ./inventory/hosts
+
 # ansible-playbook ./playbooks/07-cleanup-master.yml /inventory/hosts -u pkv
-ansible-playbook ./playbooks/08-cleanup2.yml -i ./inventory/hosts -u pkv 
+# ansible-playbook ./playbooks/08-cleanup2.yml -i ./inventory/hosts -u pkv 
 
--- kubernetes/setups/lb/lb.sh
+++ kubernetes/setups/lb/lb.sh
@@ -14,4 +14,4 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-kubectl apply -f kubernetes/setups/lb/config.yml
+kubectl apply -f config.yml
